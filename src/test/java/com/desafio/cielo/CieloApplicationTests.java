671299977a8d9b262e3e8114e65ac32e1426c9ef
taiane94sb <taiane19sb94@gmail.com>
2023-10-17 10:49:11 -0300

    initialize the project using spring initializr

---
+++ src/test/java/com/desafio/cielo/CieloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.cielo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CieloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
